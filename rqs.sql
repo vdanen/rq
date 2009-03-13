7d6739743c9b969b7c74039b0249310dac9571c3
Vincent Danen <vdanen@linsec.ca>
2009-03-13 03:20:18 +0000

    adding files to the database from src.rpm now works

--- rqs.sql
+++ rqs.sql
@@ -1,5 +1,78 @@
-DROP TABLE tags, packages, sources, files;
-CREATE TABLE tags ( tag TEXT PRIMARY KEY, path TEXT UNIQUE, tdate TEXT );
-CREATE TABLE packages ( record SERIAL PRIMARY KEY, tag TEXT, package TEXT, version TEXT, release TEXT, pdate TEXT );
-CREATE TABLE sources ( record INTEGER, type TEXT, srecord SERIAL PRIMARY KEY, pfile TEXT );
-CREATE TABLE files ( record INTEGER, srecord INTEGER, sfile TEXT );
+-- phpMyAdmin SQL Dump
+-- version 3.1.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 03, 2009 at 03:59 PM
+-- Server version: 5.0.58
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `rqs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
+  `f_id` int(24) NOT NULL auto_increment,
+  `p_record` int(24) NOT NULL,
+  `s_record` int(24) NOT NULL,
+  `sfile` TEXT NOT NULL,
+  PRIMARY KEY  (`f_id`),
+  KEY `rec` USING BTREE (`p_record`),
+  KEY `source` USING BTREE (`s_record`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packages`
+--
+
+DROP TABLE IF EXISTS `packages`;
+CREATE TABLE IF NOT EXISTS `packages` (
+  `p_record` int(24) NOT NULL auto_increment,
+  `p_tag` text NOT NULL,
+  `p_package` text NOT NULL,
+  `p_version` text NOT NULL,
+  `p_release` text NOT NULL,
+  `p_date` text NOT NULL,
+  PRIMARY KEY  (`p_record`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `provides`
+--
+
+DROP TABLE IF EXISTS `sources`;
+CREATE TABLE IF NOT EXISTS `sources` (
+  `s_id` int(24) NOT NULL auto_increment,
+  `p_record` int(24) NOT NULL,
+  `s_type` varchar(1) NOT NULL,
+  `s_file` text NOT NULL,
+  PRIMARY KEY  (`s_id`),
+  KEY `rec` USING BTREE (`p_record`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE IF NOT EXISTS `tags` (
+  `tag` varchar(128) NOT NULL,
+  `path` varchar(256) NOT NULL,
+  `tdate` text NOT NULL,
+  PRIMARY KEY  (`tag`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
