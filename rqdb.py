439b9cb165f8a447b01d8adb87decbdafa93c018
Vincent Danen <vdanen@linsec.ca>
2009-10-08 04:03:35 +0000

    break out db-related functions into rqdb and rename functions accordingly

---
+++ rqdb.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+"""
+This program extracts data from RPM and SRPM packages and stores it in
+a database for later querying.
+
+based on the srpm script of similar function copyright (c) 2005 Stew Benedict <sbenedict@mandriva.com>
+copyright (c) 2007-2009 Vincent Danen <vdanen@linsec.ca>
+
+$Id: rqp 314 2009-09-04 21:31:39Z vdanen@ANNVIX.CA $
+"""
+import MySQLdb, MySQLdb.cursors
+import logging
+
+def connect(type, config):
+    """
+    function db_connect(type, config)
+
+    Function to connect to the database.  It requires two arguments: type is
+    'source' or 'binary' to know which database we are working with and config
+    is the database configuration information.  It returns two objects: the db
+    object for connecting to the database and a modified config list that has
+    the appropriate database name (for whether it is called with type 'source'
+    or type 'binary').
+    """
+
+    if config['hostspec'] == '':
+        logging.critical('Missing hostspec in the configuration!')
+        sys.exit(1)
+    if config['username'] == '':
+        logging.critical('Missing username in the configuration!')
+        sys.exit(1)
+    if config['password'] == '':
+        logging.critical('Missing password in the configuration!')
+        sys.exit(1)
+
+    if type == 'binary':
+        if (config['rpm_database'] == ''):
+            logging.critical('Missing rpm_database in the configuration!')
+            sys.exit(1)
+        else:
+            config['database'] = config['rpm_database']
+
+    if type == 'source':
+        if (config['srpm_database'] == ''):
+            logging.critical('Missing srpm_database in the configuration!')
+            sys.exit(1)
+        else:
+            config['database'] = config['srpm_database']
+
+    logging.debug('Using host=>%s, user=>%s, db=>%s' % (config['hostspec'], config['username'], config['database']))
+
+    try:
+        db = MySQLdb.connect(host=config['hostspec'],
+                             user=config['username'],
+                             passwd=config['password'],
+                             db=config['database'],
+                             cursorclass=MySQLdb.cursors.DictCursor)
+    except MySQLdb.Error, e:
+        logging.critical('MySQL error %d: %s' % (e.args[0], e.args[1]))
+        sys.exit(1)
+
+    return (db, config)
+
+
+def close(db):
+    """
+    function db_close()
+
+    Function to close connections to the database
+    """
+    db.commit()
+    db.close()
+
+
+def fetch_all(db, query):
+    """
+    function db_fetchall(query)
+
+    Function to perform database queries.  It takes one argument, the query to
+    execute (a SELECT statement), and returns the results of the query if the
+    query was successful, returns False if not.  This function is meant to be
+    used with multi-result queries.
+    """
+    logging.debug('  in db_fetchall()')
+    logging.debug('  => query is: %s' % query)
+
+    try:
+        cursor = db.cursor()
+        cursor.execute("set autocommit=0")
+        cursor.execute(query)
+        results = cursor.fetchall()
+        cursor.close()
+
+        if results:
+            return results
+        else:
+            return False
+    except MySQLdb.Error, e:
+        logging.critical('MySQL error %d: %s' % (e.args[0], e.args[1]))
+        sys.exit(1)
+
+
+def fetch_one(db, query):
+    """
+    function db_fetchone(query)
+
+    Function to perform database queries.  It takes one argument, the query to
+    execute (a SELECT statement), and returns the results of the query if the
+    query was successful, returns False if not.  This function is meant to be
+    used with single-result queries.
+    """
+    logging.debug('  in db_fetchone()')
+    logging.debug('  => query is: %s' % query)
+
+    try:
+        cursor  = db.cursor()
+        cursor.execute(query)
+        results = cursor.fetchone()
+        cursor.close()
+        if results:
+            key = results.keys()
+            return(results[key[0]])
+        else:
+            return False
+    except MySQLdb.Error, e:
+        logging.critical('MySQL error %d: %s' % (e.args[0], e.args[1]))
+        sys.exit(1)
+
+
+def do_query(db, query):
+    """
+    Function to perform an actual update (UPDATE/INSERT) query (non-SELECT) from the database
+    """
+    logging.debug('  in db_doquery()')
+    logging.debug('  => query is: %s' % query)
+
+    try:
+        cursor = db.cursor()
+        cursor.execute(query)
+        cursor.close()
+    except MySQLdb.Error, e:
+        logging.critical('MySQL error %d: %s' % (e.args[0], e.args[1]))
+        sys.exit(1)
