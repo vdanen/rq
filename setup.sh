2797aa410835fe4324319e126b8ed04b67266473
Vincent Danen <vdanen@linsec.ca>
2017-05-14 15:14:59 -0600

    simple setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+exit=0
+for app in pip3 virtualenv; do # mysql_config; do
+    if [ "$(which ${app} >/dev/null 2>&1; echo $?)" == "1" ]; then
+        echo "Unable to find ${app} which is required!"
+        exit=1
+    fi
+done
+
+if [ ${exit} -eq 1 ]; then
+    exit 1
+fi
+
+virtualenv flask
+flask/bin/pip install -r requirements.txt
+#flask/bin/pip install mysql-python
+
