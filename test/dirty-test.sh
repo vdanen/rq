c778575801cae327210922a50dbadf116cb93359
Vincent Danen <vdanen@linsec.ca>
2017-06-10 16:55:48 -0600

    try to make the testing a little more useful

--- test/dirty-test.sh
+++ test/dirty-test.sh
@@ -1,25 +1,43 @@
 #!/bin/sh
+
+test() {
+    cmd="${1}"
+    echo ""
+    echo "TESTING:: ${cmd}"
+    ${cmd}
+    if [ "$?" != "0" ]; then
+        echo "[FAIL] SOMETHING WENT WRONG! RE-RUNNING WITH --DEBUG OPTION"
+        ${cmd} --debug
+        exit 1
+    else
+        echo "[OK]"
+    fi
+}
+
 cd ..
 echo "drop database rq_binary; create database rq_binary; drop database rq_source; create database rq_source;" | mysql -u rq
 ./create_database.py
+[[ "$?" != "0" ]] && exit 1
+
 echo "******************************************"
 echo "Testing rqp"
 echo "******************************************"
-./rqp -x
-./rqp -C test/rpm/main -U test/rpm/updates -t test -P
-./rqp -x
-./rqp --list-to-update -t test
-./rqp -u test -P
-./rqp -x
-./rqp -l
+test "./rqp -x"
+test "./rqp -C test/rpm/main -U test/rpm/updates -t test -P"
+test "./rqp -x"
+test "./rqp --list-to-update -t test"
+test "./rqp -u test -P"
+test "./rqp -x"
+test "./rqp -l"
+
 echo "******************************************"
 echo "Testing rqs"
 echo "******************************************"
-./rqs -x
-./rqs -C test/srpm/main -U test/srpm/updates -t test -P
-./rqs -x
-./rqs --list-to-update -t test
-./rqs -u test -P
-./rqs -x
-./rqs -l
+test "./rqs -x"
+test "./rqs -C test/srpm/main -U test/srpm/updates -t test -P"
+test "./rqs -x"
+test "./rqs --list-to-update -t test"
+test "./rqs -u test -P"
+test "./rqs -x"
+test "./rqs -l"
 
