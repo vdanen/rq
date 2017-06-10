66d3cea49f59589ca7824d7dd82b3e5925675c23
Vincent Danen <vdanen@linsec.ca>
2017-06-10 15:45:41 -0600

    update copyright dates
    It blows my mind that I started working on this a decade ago...

--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
+"""
+This program extracts data from RPM and SRPM packages and stores it in
+a database for later querying.
+
+based on the srpm script of similar function copyright (c) 2005 Stew Benedict <sbenedict@mandriva.com>
+copyright (c) 2007-2017 Vincent Danen <vdanen@linsec.ca>
+
+This file is part of rq.
+
+rq is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+rq is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with rq.  If not, see <http://www.gnu.org/licenses/>.
+"""
 from peewee import *
 from app import RPM_URI, SRPM_URI
 from playhouse.db_url import connect
